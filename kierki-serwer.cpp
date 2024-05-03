2446f37e2eb7fa7329b8bacf53cc6b6bef3157d3
Gustaw Blachowski <gb448194@students.mimuw.edu.pl>
2024-05-03 19:17:26 +0200

    Prototype of th connection-handling thread.

--- kierki-serwer.cpp
+++ kierki-serwer.cpp
@@ -3,6 +3,7 @@ namespace po = boost::program_options;
 
 #include <iostream>
 #include <iterator>
+#include <exception>
 #include <string>
 
 #include "serwer.h"
@@ -11,21 +12,63 @@ using std::cout;
 using std::cerr;
 using std::string;
 using std::vector;
+using std::exception;
+using std::invalid_argument;
 
 int main(int argc, char* argv[])
 {
-    po::options_description desc("Allowed options");
-    desc.add_options()
-        ("help", "produce help message")
-        (",p", po::value<vector<int>>()->multitoken(), "port number")
-        (",f", po::value<vector<string>>()->multitoken(), "file name")
-        (",t", po::value<vector<int>>()->multitoken(), "timeout");
+    int port = 0;
+    string game_file_name;
+    int timeout = 5;
+    try 
+    {
+        po::options_description desc("Allowed options");
+        desc.add_options()
+            (",p", po::value<vector<int>>()->multitoken(), "port number")
+            (",f", po::value<vector<string>>()->multitoken(), "game file name")
+            (",t", po::value<vector<int>>()->multitoken(), "timeout");
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
 
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);
+        if (vm.count("-p")) 
+        {
+            port = vm["-p"].as<vector<int>>()[0];
+            if (port < 0) 
+            {
+                throw invalid_argument("Port number must be non-negative");
+            }
+        }
 
-    serwer::Serwer s;
+        if (vm.count("-f")) 
+        {
+            game_file_name = vm["-f"].as<vector<string>>()[0];
+        }
+        else 
+        {
+            throw invalid_argument("Game file name must be provided");
+        }
+
+        if (vm.count("-t")) 
+        {
+            timeout = vm["-t"].as<vector<int>>()[0];
+            if (timeout < 0) 
+            {
+                throw invalid_argument("Timeout must be non-negative");
+            }
+        }
+    }
+    catch(exception& e) 
+    {
+        cerr << "error: " << e.what() << "\n";
+        return 1;
+    }
+    catch(...) 
+    {
+        cerr << "Exception of unknown type!\n";
+    }
+
+    serwer::Serwer s(port, timeout, game_file_name);
 
     return 0;
 }
--- serwer.h
+++ serwer.h
@@ -1,28 +1,92 @@
 #include <iostream>
 #include <thread>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <vector>
+#include <mutex>
+#include <array>
 
 namespace serwer 
 {
     using std::thread;
+    using std::mutex;
+    using std::array;
+    using std::string;
+    using std::vector;
     using std::cout;
     class Serwer
     {
     private:
         thread connection_thread;
     public:
-        Serwer();
+        Serwer() = delete;
+        Serwer(int port, int timeout, const std::string& game_file_name);
         ~Serwer();
+
+    private:
+        void handle_connections();
+
+        int port;
+        int timeout;
+        string game_file_name;
+
+        thread connection_manager_thread;
+        array<int, 4> socket_fds;
     };
 
-    inline Serwer::Serwer()
+    inline Serwer::Serwer(int port, int timeout, const std::string& game_file_name)
+        : port(port), timeout(timeout), game_file_name(game_file_name), socket_fds({-1, -1, -1, -1})
     {
-        cout << "Creating thread\n";
-        connection_thread = thread([]() { cout << "Hello from serwer\n"; });
+        connection_manager_thread = thread(&Serwer::handle_connections, this);
+
+        
     }
 
     inline Serwer::~Serwer()
     {
-        connection_thread.join();
-        cout << "Joined thread\n";
+        connection_manager_thread.join();
+    }
+
+    inline void Serwer::handle_connections()
+    {
+        int socket_fd = socket(AF_INET6, SOCK_STREAM, 0);
+        if (socket_fd == -1)
+        {
+            cout << "Error creating socket\n";
+            exit(1);
+        }
+
+        // Bind the socket to a concrete address.
+        struct sockaddr_in6 server_address;
+        server_address.sin6_family = AF_INET6; // IPv6
+        server_address.sin6_addr = IN6ADDR_ANY_INIT; // Listening on all interfaces.
+        server_address.sin6_port = htons(port);
+
+        if (bind(socket_fd, (struct sockaddr *) &server_address, (socklen_t) sizeof server_address) < 0) {
+            cout << "Error binding socket\n";
+            exit(1);
+        }
+
+        // Switch the socket to listening.
+        if (listen(socket_fd, 10) < 0) {
+            cout << "Error listening on socket\n";
+            exit(1);
+        }
+
+        for (;;) 
+        {
+            struct sockaddr_in client_address;
+            socklen_t client_address_len = sizeof(client_address);
+            int client_fd = accept(socket_fd, (struct sockaddr *) &client_address,
+                                &client_address_len);
+            if (client_fd < 0) 
+            {
+                cout << "Error accepting connection\n";
+                exit(1);
+            }
+
+            cout << "Connection accepted\n";
+        }
     }
 } // namespace serwer
