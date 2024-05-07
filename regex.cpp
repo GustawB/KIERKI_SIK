2861402533cad0d52660e414397e20110c7b225c
Gustaw Blachowski <gb448194@students.mimuw.edu.pl>
2024-05-07 21:15:29 +0200

    Implement regexes.

--- regex.cpp
+++ regex.cpp
@@ -1,21 +1,5 @@
 #include "regex.h"
 
-bool has_unique_chars(const string& s)
-{
-    for (size_t i = 0; i < s.size(); ++i)
-    {
-        for (size_t j = i + 1; j < s.size(); ++j)
-        {
-            if (s[i] == s[j])
-            {
-                return false;
-            }
-        }
-    }
-
-    return true;
-}
-
 bool regex::IAM_check(const std::string& s)
 {
     boost::regex IAM_regex("IAM[NESW]\\r\\n");
@@ -25,19 +9,47 @@ bool regex::IAM_check(const std::string& s)
 bool regex::BUSY_check(const std::string& s)
 {
     boost::regex BUSY_regex("BUSY([NESW]{0,4})\\r\\n");
-
-    if (!boost::regex_match(s, BUSY_regex))
-    {
-        return false;
-    }
-    else
-    {
-        string seats = s.substr(4, s.find('\r') - 1);
-        return has_unique_chars(seats);
-    }
+    return boost::regex_match(s, BUSY_regex);
 }
 
 bool regex::DEAL_check(const std::string& s)
 {
-    boost::regex DEAL_regex("DEAL[1-7][NESW]([][NESW]{0,4})\\r\\n");
+    boost::regex DEAL_regex("DEAL[1-7][NESW]([2-9]|10|[JQKA][CDHS]){13}\\r\\n");
+    return boost::regex_match(s, DEAL_regex);
+}
+
+bool regex::TRICK_check(const std::string& s)
+{
+    boost::regex TRICK_regex("TRICK[1-13]([NESW][CDHS]){0-3}\\r\\n");
+    return boost::regex_match(s, TRICK_regex);
+}
+
+bool regex::TRICK_client_check(const std::string& s)
+{
+    boost::regex TRICK_regex("TRICK[1-13]([NESW][CDHS]){0-3}\\r\\n");
+    return boost::regex_match(s, TRICK_regex);
+}
+
+bool regex::WRONG_check(const std::string& s)
+{
+    boost::regex WRONG_regex("WRONG[1-13]\\r\\n");
+    return boost::regex_match(s, WRONG_regex);
+}
+
+bool regex::TAKEN_check(const std::string& s)
+{
+    boost::regex TAKEN_regex("TAKEN[1-13]([2-9]|10|[JQKA][CDHS]){4}[NESW]\\r\\n");
+    return boost::regex_match(s, TAKEN_regex);
+}
+
+bool regex::SCORE_check(const std::string& s)
+{
+    boost::regex SCORE_regex("SCORE([NESW]\\b\\d+\\b){4}\\r\\n");
+    return boost::regex_match(s, SCORE_regex);
+}
+
+bool regex::TOTAL_check(const std::string& s)
+{
+    boost::regex TOTAL_regex("TOTAL([NESW]\\b\\d+\\b){4}\\r\\n");
+    return boost::regex_match(s, TOTAL_regex);
 }
--- regex.h
+++ regex.h
@@ -5,7 +5,13 @@ namespace regex
 {
     using std::string;
 
-    bool IAM_check(const std::string& s);
-    bool BUSY_check(const std::string& s);
-    bool DEAL_check(const std::string& s);
+    bool IAM_check(const string& s);
+    bool BUSY_check(const string& s);
+    bool DEAL_check(const string& s);
+    bool TRICK_check(const string& s);
+    bool TRICK_client_check(const string& s);
+    bool WRONG_check(const string& s);
+    bool TAKEN_check(const string& s);
+    bool SCORE_check(const string& s);
+    bool TOTAL_check(const string& s);
 } // namespace regex
